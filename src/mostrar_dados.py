fca3e2aaed7c7eb2a6ef4f46a5a0ce68afa8d133
Mateus Pinheiro <mateus.mcpa@gmail.com>
2025-08-28 08:43:33 -0300

    :wrench: feat: corrige df na métrica

--- src/mostrar_dados.py
+++ src/mostrar_dados.py
@@ -4,9 +4,9 @@ import streamlit as st
 def mostrar_dados(df=None):
     col1, col2 = st.columns(2)
     with col1:
-        st.metric('Quantidade de letras',df.shape[0])
+        st.metric('Quantidade de caracteres',df.shape[0])
     with col2:
-        #st.metric('Quantidade de palavras',df['palavra'].nunique())
+        st.metric('Maior frequência',df['frequencia'].max())
 
     st.bar_chart(df, x='caracter', y='frequencia', horizontal=False)
 
