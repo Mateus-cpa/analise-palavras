149b9ef292f310ecd074f3facbc5d70fbbed1c8d
Mateus Pinheiro <mateus.mcpa@gmail.com>
2025-08-29 09:19:26 -0300

    :wrench: feat: ajustada a lista_palavras

--- src/main.py
+++ src/main.py
@@ -1,18 +1,23 @@
 import streamlit as st
+import pandas as pd
 
-from request_palavras import importar_palavras, contar_caracteres
+from request_palavras import importar_palavras 
 from analise_palavras import analise_caracteres, analise_palavras
 from mostrar_dados import mostrar_dados
 
 
 def tela_streamlit():
     st.title('Análise de palavras em português brasileiro')
+    # -- Importação e atualização
     with st.expander('Atualizar base de dados'):
-        importar_palavras()
-        st.session_state.df_caracteres = contar_caracteres(lista_palavras=st.session_state.lista_palavras)
-    st.subheader('Análise das letras')
+        importar = st.button('Atualizar base de dados')
+        if importar:
+            df_caracteres = importar_palavras()
+        else:
+            df_caracteres = pd.read_csv('data/frequencia_caracteres.csv', encoding='latin-1')
+    
     # -- tratamento --
-    df_caracteres = analise_caracteres(st.session_state.df_caracteres)
+    df_caracteres = analise_caracteres(df_caracteres)
 
     # -- mostrar dados caracteres --
     mostrar_dados(df_caracteres)
--- src/request_palavras.py
+++ src/request_palavras.py
@@ -7,32 +7,30 @@ import streamlit as st
 def importar_palavras():
     """
     Importa a lista de palavras do site da USP.
+    Depois, processa a lista de palavras e retorna um DataFrame com a frequência de caracteres.
+
 
     """
-    importar = st.button('Atualizar base de dados')
-    if importar:
-        try:
-            resposta = re.get('https://www.ime.usp.br/~pf/dicios/br-utf8.txt')
-        except Exception as e:
-            st.error(f'Erro ao acessar o site: {e}')
-            return []
 
-        conteudo_site = resposta.text.encode(encoding='utf-8').decode(encoding='utf-8')
+    try:
+        resposta = re.get('https://www.ime.usp.br/~pf/dicios/br-utf8.txt')
+    except Exception as e:
+        st.error(f'Erro ao acessar o site: {e}')
+        return []
 
-        st.write(f'tipo conteúdo site: {type(conteudo_site)}')
-        lista_palavras = conteudo_site.split('\n')
-        st.success(f"primeiras palavras: {lista_palavras[:5]}")
-        st.success(f'quantidade de palavras: {len(lista_palavras)}')
-        st.success('Base de dados atualizada com sucesso!')
+    conteudo_site = resposta.text.encode(encoding='utf-8').decode(encoding='utf-8')
 
-        st.session_state.lista_palavras = lista_palavras
+    st.write(f'tipo conteúdo site: {type(conteudo_site)}')
+    lista_palavras = conteudo_site.split('\n')
+    st.success(f"primeiras palavras: {lista_palavras[:5]}")
+    st.success(f'quantidade de palavras: {len(lista_palavras)}')
+    st.success('Base de dados atualizada com sucesso!')
 
-def contar_caracteres(lista_palavras: list = None):
-    """
-    Processa a lista de palavras e retorna um DataFrame com a frequência de caracteres.
-    Args:
-        lista_palavras (list): Lista de palavras a serem processadas.
-    """
+    # salvar lista em csv
+    df_palavras: pd.DataFrame = pd.DataFrame(lista_palavras, columns=['palavra'])
+    df_palavras.to_csv('data/palavras_portugues.csv', index=False, encoding='latin-1')
+
+
     st.success('Iniciando processamento das palavras...')
     tempo_inicial = time.time()
 
@@ -58,4 +56,3 @@ def contar_caracteres(lista_palavras: list = None):
 
 if __name__ == "__main__":
     importar_palavras()
-    contar_caracteres()
