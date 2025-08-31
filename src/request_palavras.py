b0b2bb1844bd372718967a0e5413d80179944aab
Mateus Pinheiro <mateus.mcpa@gmail.com>
2025-08-31 14:37:31 -0300

    :sparkles: feat: valor de palavra implementado

--- src/request_palavras.py
+++ src/request_palavras.py
@@ -38,7 +38,7 @@ def importar_palavras():
 
     # salvar lista em csv
     df_palavras: pd.DataFrame = pd.DataFrame(lista_palavras, columns=['palavra']).dropna()
-    df_palavras.to_csv('data/palavras_portugues.csv', index=False, encoding='latin-1')
+    #df_palavras = df_palavras[:100]
 
     st.success('Iniciando processamento das palavras...')
     
@@ -51,17 +51,40 @@ def importar_palavras():
     st.code(frequencia_caracteres)    
 
     # calcula tamanho das palavras
+    st.success('Calculando tamanho das palavras...')
+
     df_palavras['tamanho'] = df_palavras['palavra'].str.len()
-
-    tempo_final = time.time()
-    st.success(f'tempo corrido: {tempo_final-tempo_inicial:.2f} segundos')
-
+    
     df_frequencia = pd.DataFrame(list(frequencia_caracteres.items()), columns=['caracter', 'frequencia'])
     df_frequencia.to_csv('data/frequencia_caracteres.csv', index=False, encoding='latin-1')
 
     # calcula valor a palavra com letras considerando a frequência dos caracteres
-    df_palavras['valor_palavra'] = df_palavras['palavra'].apply(calcular_valor_palavra)
+    st.success('Calculando valor das palavras...')
+    #progresso
+    progresso = st.progress(0)
+    total_palavras = len(df_palavras)
+    percentual_text = st.empty()
+    for i, palavra in enumerate(df_palavras['palavra']):
+        try:
+            df_palavras.at[i, 'valor_palavra'] = calcular_valor_palavra(palavra)
+        except Exception as e:
+            st.error(f'Erro ao calcular valor da palavra {palavra}: {e}')
+            df_palavras.at[i, 'valor_palavra'] = 0
+        percentual = ((i + 1) / total_palavras) * 100
+        progresso.progress((i + 1) / total_palavras)
+        percentual_text.text(f'Progresso: {percentual:.2f}%')
+    st.write(df_palavras.sample(10))
+
+    # salvar arquivo atualizado
     df_palavras.to_csv('data/palavras_portugues.csv', index=False, encoding='latin-1')
+    st.success('Arquivo atualizado com sucesso!')
+
+    tempo_final = time.time()
+    tempo_decorrido = tempo_final-tempo_inicial
+    if tempo_decorrido <= 60:
+       st.success(f'tempo corrido: {tempo_decorrido:.2f} segundos.')
+    else:
+        st.success(f'tempo corrido: {tempo_decorrido/60:.2f} minutos.')
 
     return df_frequencia
 
